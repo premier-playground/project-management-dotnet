5ae0319fcd0cc25236547c27d7fafb55bd07cf3f
Kamilla <kamillaveronika@gmail.com>
2023-05-30 09:09:01 -0300

    created webapi project

---
+++ ProjectManagement.Repositories/Repositories/IProjectRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectManagement.Entities.Models;
+
+namespace ProjectManagement.Repositories.Repositories
+{
+    public interface IProjectRepository: IDisposable
+    {
+
+        Project InsertProject(Project project);
+        void UpdateProject(Project project);
+        void DeleteStudent(Project project);
+        IEnumerable<Project> GetAllProjects();
+        Project GetProjectById(string id);
+    }
+}
---
+++ ProjectManagement.WebApi/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Domain.DTO;
+using ProjectManagement.Domain.Services;
+using ProjectManagement.Entities.Models;
+using ProjectManagement.Repositories.Contexts;
+
+
+namespace ProjectManagement.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController: ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController ()
+        {
+            this._userService = new UserService(new LocalDBContext());
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(StudentDTO studentDto)
+        {
+            IActionResult httpActionResult;
+            try
+            {
+                Student student = _userService.CreateStudent(studentDto);
+                httpActionResult = new OkObjectResult(student);
+            }
+            catch (Exception)
+            {
+                httpActionResult = BadRequest();
+            }
+            
+            return httpActionResult;
+        }
+
+    }
+}
