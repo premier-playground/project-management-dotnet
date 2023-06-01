9640f6922f30e2fe48276d3e7906e60a7cd907bb
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-06-01 16:13:09 -0300

    add create student test

---
+++ ProjectManagement.Tests/ServicesTest/UserServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectManagement.Domain.DTO;
+using ProjectManagement.Domain.Services;
+using ProjectManagement.Entities.Enums;
+using ProjectManagement.Entities.Models;
+using ProjectManagement.Repositories.Contexts;
+
+namespace ProjectManagement.Tests.ServicesTest
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        private readonly UserService _userService;
+
+        public UserServiceTest()
+        {
+            _userService = new UserService(new LocalDBContext());
+        }
+
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            using (var localDbContext = new LocalDBContext())
+            {
+                localDbContext.StudentProjectAssociations.RemoveRange(localDbContext.StudentProjectAssociations);
+                localDbContext.Professors.RemoveRange(localDbContext.Professors);
+                localDbContext.Students.RemoveRange(localDbContext.Students);
+                localDbContext.Projects.RemoveRange(localDbContext.Projects);
+            }
+        }
+
+        [TestMethod]
+        public void SuccessfulCreateStudent()
+        {
+            StudentDTO studentDTO = new StudentDTO
+            {
+                Email = "davi.sousa@ccc.ufcg.edu.br",
+                Name = "Davi", 
+                Password = "123456",
+                Role = Role.STUDENT, 
+                Institution = "UFCG"
+            };
+
+            var createdStudent = _userService.CreateStudent(studentDTO);
+            Assert.IsTrue(createdStudent.UserName == studentDTO.Name);
+        }
+    }
+}
